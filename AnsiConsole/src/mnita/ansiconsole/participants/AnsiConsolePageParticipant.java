d51730686ddd18a4a198751657b925c6a7384654
Mihai Nita <nmihai_2000@yahoo.com>
2016-05-02 00:30:04 -0700

    Revert "changed the default behavior of the copy action to remove escapes"

--- AnsiConsole/src/mnita/ansiconsole/participants/AnsiConsolePageParticipant.java
+++ AnsiConsole/src/mnita/ansiconsole/participants/AnsiConsolePageParticipant.java
@@ -1,46 +1,39 @@
 package mnita.ansiconsole.participants;
 
+import mnita.ansiconsole.AnsiConsoleActivator;
+
 import org.eclipse.swt.custom.StyledText;
-import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsolePageParticipant;
 import org.eclipse.ui.part.IPageBookViewPage;
 
-import mnita.ansiconsole.AnsiConsoleActivator;
-import mnita.ansiconsole.actions.AnsiConsoleCopyToClipboardAction;
-
 public class AnsiConsolePageParticipant implements IConsolePageParticipant {
+    @Override
+    @SuppressWarnings("rawtypes")
+    public Object getAdapter(Class arg) {
+        return null;
+    }
 
-	@Override
-	@SuppressWarnings("rawtypes")
-	public Object getAdapter(Class arg) {
-		return null;
-	}
+    @Override
+    public void activated() {
+    }
 
-	@Override
-	public void activated() {
-	}
+    @Override
+    public void deactivated() {
+    }
 
-	@Override
-	public void deactivated() {
-	}
+    @Override
+    public void dispose() {
+        AnsiConsoleActivator.getDefault().removeViewerWithPageParticipant(this);
+    }
 
-	@Override
-	public void dispose() {
-		AnsiConsoleActivator.getDefault().removeViewerWithPageParticipant(this);
-	}
-
-	@Override
-	public void init(IPageBookViewPage page, IConsole console) {
-		if (page.getControl() instanceof StyledText) {
-			StyledText viewer = (StyledText) page.getControl();
-			AnsiConsoleStyleListener myListener = new AnsiConsoleStyleListener();
-			viewer.addLineStyleListener(myListener);
-			AnsiConsoleActivator.getDefault().addViewer(viewer, this);
-			
-			AnsiConsoleCopyToClipboardAction copyAction = new AnsiConsoleCopyToClipboardAction(viewer);
-			page.getSite().getActionBars().setGlobalActionHandler(ActionFactory.COPY.getId(), copyAction);
-			page.getSite().getActionBars().updateActionBars();
-		}
-	}
+    @Override
+    public void init(IPageBookViewPage page, IConsole console) {
+        if (page.getControl() instanceof StyledText) {
+            StyledText viewer = (StyledText) page.getControl();
+            AnsiConsoleStyleListener myListener = new AnsiConsoleStyleListener();
+            viewer.addLineStyleListener(myListener);
+            AnsiConsoleActivator.getDefault().addViewer(viewer, this);
+        }
+    }
 }

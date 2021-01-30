7b7573df17d0d97c0d1d299c968b1235c7421965
Mihai Nita <nmihai_2000@yahoo.com>
2021-01-30 01:44:41 -0800

    Add links to my web site, GitHub, Eclipse Marketplace

--- AnsiConsole/src/mnita/ansiconsole/preferences/AnsiConsolePreferencePage.java
+++ AnsiConsole/src/mnita/ansiconsole/preferences/AnsiConsolePreferencePage.java
@@ -1,11 +1,17 @@
 package mnita.ansiconsole.preferences;
 
+import java.net.URL;
+
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PlatformUI;
 
 import mnita.ansiconsole.AnsiConsoleActivator;
 import mnita.ansiconsole.utils.AnsiConsoleColorPalette;
@@ -49,10 +55,37 @@ public class AnsiConsolePreferencePage extends FieldEditorPreferencePage impleme
                         { "Ubuntu", AnsiConsoleColorPalette.PALETTE_UBUNTU }
                 },
                 parent));
+
+        createLink(parent, true, true, "<a href=\"https://mihai-nita.net/2013/06/03/eclipse-plugin-ansi-in-console/\">Home page</a>:"
+                + " some documentation, release notes, etc.");
+        createLink(parent, false, false, "<a href=\"https://github.com/mihnita/ansi-econsole/\">GitHub page</a>:"
+                + " source code, report issues, etc.");
+        createLink(parent, true, false, "<a href=\"https://marketplace.eclipse.org/content/ansi-escape-console\">Eclipse Marketplace</a>:"
+                + " give it a star / review if you like it :-).");
     }
 
     @Override
     public void init(IWorkbench workbench) {
         // Nothing to do, but we are forced to implement it for IWorkbenchPreferencePage
+    }
+
+    private void createLink(Composite parent, boolean fillGap, boolean endAlign, String text) {
+        Link link = new Link(parent, SWT.WRAP);
+        link.setText(text);
+        GridData gridData = new GridData();
+        gridData.horizontalSpan = 2;
+        if (fillGap) {
+        	gridData.grabExcessVerticalSpace = true;
+        	if (endAlign)
+        		gridData.verticalAlignment = SWT.END;
+        	else
+        		gridData.verticalAlignment = SWT.TOP;
+    	}
+        link.setLayoutData(gridData);
+        link.addListener(SWT.Selection, event -> {
+            try {
+                PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser().openURL(new URL(event.text));
+            } catch (Exception e) {}
+        });
     }
 }

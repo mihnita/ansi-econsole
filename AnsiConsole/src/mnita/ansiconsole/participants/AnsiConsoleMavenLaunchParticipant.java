9b627b5e8bebed399434356944152a3ac6ab7196
Mihai Nita <nmihai_2000@yahoo.com>
2022-06-01 18:37:05 -0700

    Don't force maven color if the plugin is disabled

--- AnsiConsole/src/mnita/ansiconsole/participants/AnsiConsoleMavenLaunchParticipant.java
+++ AnsiConsole/src/mnita/ansiconsole/participants/AnsiConsoleMavenLaunchParticipant.java
@@ -9,23 +9,28 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.sourcelookup.ISourceLookupParticipant;
 import org.eclipse.m2e.internal.launch.IMavenLaunchParticipant;
 
+import mnita.ansiconsole.preferences.AnsiConsolePreferenceUtils;
+
 @SuppressWarnings("restriction")
 public class AnsiConsoleMavenLaunchParticipant implements IMavenLaunchParticipant {
     // Force in code what was done by hand here:
     //   https://mihai-nita.net/2020/08/23/force-maven-colors-in-eclipse-console/
 
     @Override
-    public String getProgramArguments(ILaunchConfiguration arg0, ILaunch arg1, IProgressMonitor arg2) {
-        return "-Dstyle.color=always";
+    public String getProgramArguments(
+    		ILaunchConfiguration launchCfg, ILaunch launch, IProgressMonitor progMonitor) {
+    	return AnsiConsolePreferenceUtils.isAnsiConsoleEnabled() ? "-Dstyle.color=always" : "";
     }
 
     @Override
-    public List<ISourceLookupParticipant> getSourceLookupParticipants(ILaunchConfiguration arg0, ILaunch arg1, IProgressMonitor arg2) {
+    public List<ISourceLookupParticipant> getSourceLookupParticipants(
+    		ILaunchConfiguration launchCfg, ILaunch launch, IProgressMonitor progMonitor) {
         return new ArrayList<>();
     }
 
     @Override
-    public String getVMArguments(ILaunchConfiguration arg0, ILaunch arg1, IProgressMonitor arg2) {
-        return "-Djansi.passthrough=true";
+    public String getVMArguments(
+    		ILaunchConfiguration launchCfg, ILaunch launch, IProgressMonitor progMonitor) {
+    	return AnsiConsolePreferenceUtils.isAnsiConsoleEnabled() ? "-Djansi.passthrough=true" : "";
     }
 }
